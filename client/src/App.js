93c7073223023acf2d944c35b88939091eddd669
meowOtter <angie.to@berkeley.edu>
2024-04-11 14:35:44 -0700

    add `PostCreate` form

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import PostCreate from "./PostCreate";
 
 const App = () => {
-	return <div id="root">Blog app</div>;
+	return (
+		<div id="root" className="container">
+			<h1>Create Post</h1>
+			<PostCreate />
+		</div>
+	);
 };
 
 export default App;
