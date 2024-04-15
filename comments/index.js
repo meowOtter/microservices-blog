dfee8dc8dbfd8972314fd73398b4c309035e87f6
meowOtter <angie.to@berkeley.edu>
2024-04-15 14:52:21 -0700

    added events between services and the event bus

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.get("/posts/:id/comments", (req, res) => {
 	res.status(200).send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
 	const commentId = randomBytes(4).toString("hex");
 	const { comment } = req.body;
 	const postId = req.params.id;
@@ -22,7 +23,22 @@ app.post("/posts/:id/comments", (req, res) => {
 	comments.push({ id: commentId, text: comment });
 	commentsByPostId[postId] = comments;
 
+	await axios.post("http://localhost:4005/events", {
+		type: "CommentCreated",
+		data: {
+			id: commentId,
+			text: comment,
+			postId,
+		},
+	});
+
 	res.status(201).send(comments[comments.length - 1]);
+});
+
+app.post("/events", (req, res) => {
+	console.log("Received event", req.body.type);
+
+	res.send({});
 });
 
 app.listen(4001, () => {
